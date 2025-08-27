81ecf2ac0b40d3005196a9e1b95c5988d7884647
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2025-08-27 17:36:32 +0530

    Time: 1 ms (99.95%), Space: 43.6 MB (44.75%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void generate(List<List<Integer>> result,int[] nums,int start, List<Integer> list){
+        result.add(new ArrayList<>(list));//add current subset
+        for(int i=start;i<nums.length;i++){
+            if(i>start && nums[i]==nums[i-1]) continue;
+            list.add(nums[i]);
+            generate(result,nums,i+1,list);
+            list.remove(list.size()-1);
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result=new ArrayList<>();
+        generate(result,nums,0,new ArrayList<>());
+        return result;
+    }
+}
