c7be7c5141246fb593f4a3b2cdd442ce7d70d203
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2025-10-16 18:34:42 +0530

    Time: 0 ms (100%), Space: 46 MB (50.15%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,49 +1,51 @@
 class Solution {
-    public int firstoccurence(int[] nums,int target){
+    public int first(int[] nums,int target){
         int n=nums.length;
-        int low=0;
-        int high=n-1;
         int first=-1;
-        while(low<=high){
-            int mid=(low+high)/2;
+        int l=0;
+        int r=n-1;
+        while(l<=r){
+            int mid=(l+r)/2;
             if(nums[mid]==target){
                 first=mid;
-                high=mid-1;
+                r=mid-1;
             }
             else if(nums[mid]<target){
-                low=mid+1;
+                l=mid+1;
             }
             else{
-                high=mid-1;
+                r=mid-1;
             }
+
         }
         return first;
     }
-    public int lastoccurence(int[] nums,int target){
+    public int last(int[] nums,int target){
         int n=nums.length;
-        int low=0;
-        int high=n-1;
         int last=-1;
-        while(low<=high){
-            int mid=(low+high)/2;
+        int l=0;
+        int r=n-1;
+        while(l<=r){
+            int mid=(l+r)/2;
             if(nums[mid]==target){
                 last=mid;
-                low=mid+1;
+                l=mid+1;
             }
-            else if(nums[mid]<target) low=mid+1;
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
             else{
-                high=mid-1;
+                r=mid-1;
             }
+
         }
         return last;
-        
     }
     public int[] searchRange(int[] nums, int target) {
-        int first=firstoccurence(nums,target);
-        if(first ==-1) return new int[]{-1,-1};
-        int last=lastoccurence(nums,target);
-        return new int[]{first,last};
-
+        int n=nums.length;
+        int prev=first(nums,target);
+        int next=last(nums,target);
+        return new int[]{prev,next};
         
         
     }
