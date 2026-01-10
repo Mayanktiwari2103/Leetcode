30d1c4327dff770c3fe5fc4a0639c2a9bb21e068
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-01-10 14:47:22 +0530

    Time: 0 ms (100%), Space: 48.1 MB (74.45%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,11 +1,11 @@
 class Solution {
     public int first(int[] nums,int target){
         int n=nums.length;
-        int first=-1;
         int l=0;
         int r=n-1;
+        int first=-1;
         while(l<=r){
-            int mid=(l+r)/2;
+            int mid=l+(r-l)/2;
             if(nums[mid]==target){
                 first=mid;
                 r=mid-1;
@@ -16,37 +16,37 @@ class Solution {
             else{
                 r=mid-1;
             }
-
         }
         return first;
+
     }
-    public int last(int[] nums,int target){
+    
+    public int last (int[] nums,int target){
         int n=nums.length;
-        int last=-1;
         int l=0;
         int r=n-1;
+        int last=-1;
         while(l<=r){
-            int mid=(l+r)/2;
+            int mid=l+(r-l)/2;
             if(nums[mid]==target){
                 last=mid;
                 l=mid+1;
             }
-            else if(nums[mid]<target){
+            else if(nums[mid]< target){
                 l=mid+1;
             }
             else{
                 r=mid-1;
             }
-
         }
         return last;
     }
-    public int[] searchRange(int[] nums, int target) {
+     public int[] searchRange(int[] nums, int target) {
         int n=nums.length;
         int prev=first(nums,target);
         int next=last(nums,target);
-        return new int[]{prev,next};
+        return new int[] {prev,next};
         
-        
+
     }
 }
