cdc1d5718c16c29cbd3e80e3df064e68b6e8c81a
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-01-10 14:24:34 +0530

    Time: 0 ms (100%), Space: 44.9 MB (14.35%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -5,13 +5,17 @@ class Solution {
         int r=n-1;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(nums[mid]==target) return mid;
-            else if(target>nums[mid]) l=mid+1;
-            else{
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
                 r=mid-1;
+
+            }
+            else{
+                l=mid+1;
             }
         }
         return l;
-        
     }
 }
