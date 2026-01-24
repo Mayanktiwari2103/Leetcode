eaeb4825e2cc81639a04a7e91e16c3fc13f9dcef
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-01-24 23:37:10 +0530

    Time: 1 ms (99.68%), Space: 46.8 MB (54.86%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,35 +9,33 @@
  * }
  */
 class Solution {
-    public ListNode merge(ListNode list1,ListNode list2){
+    public ListNode merge(ListNode l1, ListNode l2){
         ListNode dummy=new ListNode(0);
-        ListNode res=dummy;
-        while(list1!=null&& list2!=null){
-            if(list1.val<list2.val){
-                res.next=list1;
-                list1=list1.next;
+        ListNode ans=dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                ans.next=l1;
+                l1=l1.next;
             }
             else{
-                res.next=list2;
-                list2=list2.next;
+                ans.next=l2;
+                l2=l2.next;
             }
-            res=res.next;
+            ans=ans.next;
         }
-        if(list1!=null) res.next=list1;
-        else res.next=list2;
+        if(l1!=null) ans.next=l1;
+        else ans.next=l2;
         return dummy.next;
-
     }
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists==null || lists.length==0) return null;
-        return mergehelper(lists, 0, lists.length - 1);
+        return mergehelper(lists,0,lists.length-1);
     }
-    private ListNode mergehelper(ListNode lists[],int left,int right){
-         if (left == right) return lists[left];
+    public ListNode mergehelper(ListNode lists[],int left, int right){
+        if(left==right) return lists[left];
         int mid=left+(right-left)/2;
-        ListNode l1=mergehelper(lists,left,mid);
-        ListNode l2=mergehelper(lists,mid+1,right);
-
-        return merge(l1,l2);
+        ListNode l1= mergehelper(lists,left,mid);
+        ListNode l2= mergehelper(lists,mid+1,right);
+        return  merge(l1,l2);
     }
 }
