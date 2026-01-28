ce614522a6b93032096a53bb3309edc37e4e2f97
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-01-28 17:40:41 +0530

    Time: 1 ms (87.3%), Space: 45.7 MB (25.37%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,21 +1,20 @@
 class Solution {
-    public void generate(List<List<Integer>> result,int[] nums,List<Integer> list){
-        int n=nums.length;
-        if(list.size()==n){
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result=new ArrayList<>();
+        generate(nums,result,new ArrayList<>(),0);
+        return result;
+
+    }
+    public void generate(int[] nums,List<List<Integer>> result,List<Integer> list,int start){
+        if(list.size()==nums.length){
             result.add(new ArrayList<>(list));
             return;
         }
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.length;i++){
             if(list.contains(nums[i])) continue;
             list.add(nums[i]);
-            generate(result,nums,list);
+            generate(nums,result,list,i+1);
             list.remove(list.size()-1);
-
         }
-    }
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
-        generate(result,nums,new ArrayList<>());
-        return result;
     }
 }
