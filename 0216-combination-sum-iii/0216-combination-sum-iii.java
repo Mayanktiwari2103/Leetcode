f48daf8929a55fcb6c5bc7cea1171c652ba73068
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-01-28 17:09:52 +0530

    Time: 0 ms (100%), Space: 42.1 MB (94.69%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,21 +1,22 @@
 class Solution {
-    public void generate(List<List<Integer>> ls,int k, int n,List<Integer> list,int start,int sum){
-        if(list.size()==k && sum==n){
-            ls.add(new ArrayList<>(list));
-            return ;
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result=new ArrayList<>();
+        generate(k,n,0,result,new ArrayList<>(),1);
+        return result;
+    }
+    public void generate(int k, int n , int sum, List<List<Integer>> result,List<Integer> list,int start){
+        if(list.size()==k){
+            if(sum==n){
+               result.add(new ArrayList<>(list));
+               return;
         }
-        if(sum>n) return ;
+
+        }
+        if(sum>n) return;
         for(int i=start;i<10;i++){
             list.add(i);
-            generate(ls,k,n,list,i+1,sum+i);
+            generate(k,n,sum+i,result,list,i+1);
             list.remove(list.size()-1);
-
         }
-
-    }
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> ls=new ArrayList<>();
-        generate(ls,k,n,new ArrayList<>(),1,0);
-        return ls;
     }
 }
