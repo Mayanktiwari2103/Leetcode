ee9f756b8c6935b7ba7c298232f09ae6489ac5f1
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-01-28 16:12:47 +0530

    Time: 2 ms (95.09%), Space: 45.9 MB (29.53%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,21 +1,22 @@
 class Solution {
-    public void generate(List<List<Integer>> ls,int[] candidates,int target,List<Integer> list,int start,int sum){
-        int n=candidates.length;
-         if(sum==target){
-            ls.add(new ArrayList<>(list));
-            return;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result=new ArrayList<>();
+        generate(candidates,target,result,new ArrayList<>(),0,0);
+        return result;
+    
+    }
+    public void generate(int[] candidates, int target, List<List<Integer>> result, List<Integer> list,int sum, int start ){
+        if(sum==target){
+            result.add(new ArrayList<>(list));
+
         }
-        if(sum>target) return;
-       
-        for(int i=start;i<n;i++){
+        if(sum> target) return;
+        for(int i=start;i<candidates.length;i++){
             list.add(candidates[i]);
-            generate(ls,candidates,target,list,i,sum+candidates[i]);
+            generate(candidates,target,result,list,sum+candidates[i],i);
             list.remove(list.size()-1);
         }
     }
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ls=new ArrayList<>();
-        generate(ls,candidates,target,new ArrayList<>(),0,0);
-        return ls;
-    }
-}
+
+}    
+    
