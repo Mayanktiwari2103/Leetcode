da96b07d818ab00755d9b99802a796bd62f8e635
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-01-29 18:53:16 +0530

    Time: 137 ms (58.76%), Space: 42.8 MB (92.76%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,44 +1,49 @@
 class Solution {
-    public boolean generate(char[][] board, String word, int i,int j,int start){
-        int row=board.length;
-        int col=board[0].length;
-        //base case
-        if(start==word.length()){
-            return true;
-        }
-         //out of bound
-        if(i<0 || j<0 || i>=row || j>=col){
-            return false;
-        }
-        if(board[i][j]!=word.charAt(start)){
-            return false;
-        }
-        //mark as visited
-        char ch=board[i][j];
-        board[i][j]='#';
-        boolean found=generate(board,word,i,j+1,start+1) || 
-                      generate(board,word,i+1,j,start+1) || 
-                      generate(board,word,i-1,j,start+1) || 
-                      generate(board,word,i,j-1,start+1);
-
-        //restore again if not true;
-        board[i][j]=ch;              
-        return found;
-    }
     public boolean exist(char[][] board, String word) {
-        int row=board.length;
-        int col=board[0].length;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(generate(board,word,i,j,0)){
-                    return true;
+        int n=board.length;
+        int m=board[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(word.charAt(0)==board[i][j]){
+                    boolean found=dfs(board,word,i,j,0);
+                    if(found) return true;
 
                 }
-
-
             }
         }
         return false;
+    
+        
+    }
+    public boolean dfs(char[][] board, String word, int r , int c ,int wi){
+        int n=board.length;
+        int m=board[0].length;
+        // in this function false means backtrack and true means continue;
+        //base case
+        if(wi==word.length()) return true;
+
+        //out of bound
+        if(r<0 || r>=n || c<0 || c>=m){
+            return false;
+        }
+
+        //invalid cases
+        if(board[r][c]==' ' || board[r][c]!=word.charAt(wi)){
+            return false;
+        }
+        char ch=board[r][c];
+        board[r][c]=' ';
+        if(dfs(board,word,r+1,c,wi+1)||
+           dfs(board,word,r-1,c,wi+1)||
+           dfs(board,word,r,c+1,wi+1)||
+           dfs(board,word,r,c-1,wi+1))
+        {
+            return true;
+        }
+        //backtrack
+        board[r][c]=ch;
+        return false;
+
         
     }
 }
