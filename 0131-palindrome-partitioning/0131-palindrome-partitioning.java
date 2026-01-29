92b1db1faf3d820a9154ffb88d56f14db77182af
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-01-29 07:00:13 +0530

    Time: 7 ms (98.38%), Space: 65.4 MB (50.76%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,34 +1,31 @@
 class Solution {
-    public void generate(List<List<String>> ls,List<String> current,int start,String s){
-        if(start==s.length()){
-            ls.add(new ArrayList<>(current));
-            return;
-        }
-        for(int i=start;i<s.length();i++){
-            String substring=s.substring(start,i+1);
-            if(palindrome(substring)){
-                 current.add(substring);
-                 generate(ls,current,i+1,s);
-                 current.remove(current.size()-1);
-            }
-           
-        }
+    public List<List<String>> partition(String s) {
+        List<List<String>> result=new ArrayList<>();
+        generate(s,result,new ArrayList<>(),0);
+        return result;
     }
-    public boolean palindrome(String s){
-        int l=0;
-        int r=s.length()-1;
+    public boolean palindrome(String s,int l,int r){
         while(l<r){
             if(s.charAt(l)!=s.charAt(r)){
                 return false;
+
             }
             l++;
             r--;
         }
         return true;
     }
-    public List<List<String>> partition(String s) {
-        List<List<String>> ls=new ArrayList<>();
-        generate(ls,new ArrayList<>(),0,s);
-        return ls;
+    public void generate(String s,List<List<String>> result,List<String> list,int start){
+        if(start==s.length()){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=start;i<s.length();i++){
+            if(palindrome(s,start,i)){
+                list.add(s.substring(start,i+1));
+                generate(s,result,list,i+1);
+                list.remove(list.size()-1);
+            }
+        }
     }
 }
