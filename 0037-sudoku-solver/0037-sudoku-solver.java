4e2a72f1a1889bd69764de1cc6fb57c3e32a29ae
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-01-31 15:15:46 +0530

    Time: 226 ms (52.55%), Space: 43 MB (42.25%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,56 +1,48 @@
 class Solution {
-    public boolean generate(char[][] board,int row,int col){
-        if(row==9){
-           return true;
-        }
-        int nextrow=row,nextcol=col+1;
-        if(nextcol==9){
-            nextrow=row+1;
-            nextcol=0;
+    public void solveSudoku(char[][] board) {
+        generate(board);
+    }
 
-        }
-        if(board[row][col]!='.'){
-            return generate(board,nextrow,nextcol);
-        }
-        for(char digit='1';digit<='9';digit++){
-            if(issafe(board,row,col,digit)){
-                board[row][col]=digit;
-                if(generate(board,nextrow,nextcol)) return true;
-                board[row][col]='.';
+    public boolean issafe(char[][] board, int row, int col,char ch) {
+            for (int i= 0; i < 9; i++) {
+                if (board[row][i] == ch)
+                    return false;
+
+            }
+            for (int j = 0; j < 9; j++) {
+                if (board[j][col] == ch)
+                    return false;
+            }
+            int sr=row-row%3;
+            int sc=col-col%3;
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    if(board[sr+i][sc+j]==ch) return false;
+                }
+            }
+           
+        return true;
+    }
+
+    public boolean generate(char[][] board) {
+
+        for (int i=0;i<9;i++){
+            for (int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if (issafe(board,i,j,ch)) {
+                            board[i][j] = ch;
+                            if(generate(board)) return true;;
+                            board[i][j] = '.';
+                        }
+                   
+                    }
+                    return false;
+
+                }
+
             }
         }
-        return false;
-    }
-    public boolean issafe(char[][] board,int row, int col,char digit){
-            for(int j=0;j<9;j++){
-                if(board[row][j]==digit){
-                    return false;
-                }
-
-            }            
-            for(int j=0;j<9;j++){
-                if(board[j][col]==digit){
-                    return false;
-                }
-
-            }   
-            int srow=(row/3)*3;
-            int scol=(col/3)*3;
-            for(int i=srow;i<=srow+2;i++){
-                for(int j=scol;j<=scol+2;j++){
-                    if(board[i][j]==digit){
-                        return false;
-                    }
-                }
-            }         
-        
         return true;
-
-
-
-    }
-    public void solveSudoku(char[][] board) {
-        generate(board,0,0);
-
     }
 }
