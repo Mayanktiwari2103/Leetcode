a7b5cf23f0de4ca30a8faacabb67da6d51bce746
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-02-04 18:04:29 +0530

    Time: 80 ms (10.77%), Space: 102.1 MB (94.68%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,30 +1,33 @@
 class Solution {
     public int[] nge(int[] temperatures){
         int n=temperatures.length;
-        Stack<Integer> st=new Stack();
-        int[] answer=new int[n];
+        Stack<Integer> st=new Stack<>();
+        int[] ans=new int[n];
         for(int i=n-1;i>=0;i--){
             while(!st.isEmpty() && temperatures[st.peek()]<=temperatures[i]){
                 st.pop();
             }
             if(st.isEmpty()){
-                answer[i]=i;
+                ans[i]=i;
+
             }
             else{
-                answer[i]=st.peek();
+                ans[i]=st.peek();
             }
             st.push(i);
         }
-        return answer;
+        return ans;
     }
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] ans=nge(temperatures);
         int n=temperatures.length;
-        int[] waiting=new int[n];
+        int[] result=new int[n];
+        int nextg[]=nge(temperatures);
         for(int i=0;i<n;i++){
-            waiting[i]=ans[i]-i;
+            result[i]=nextg[i]-i;
+
 
         }
-        return waiting;
+        return result;
+
     }
 }
