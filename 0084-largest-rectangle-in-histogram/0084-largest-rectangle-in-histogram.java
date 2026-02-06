922fe673d634bafd234074dccbf712342e5fcb6a
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-02-06 19:37:30 +0530

    Time: 117 ms (5.66%), Space: 76 MB (96.08%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,26 +1,7 @@
-class Solution { 
-    private int[] nse(int[] heights){
+class Solution {
+    public int[] pse(int[] heights){
         int n=heights.length;
-        Stack<Integer> st=new Stack();
-        int[] ans=new int[n];
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                ans[i]=n;
-            }
-            else{
-                ans[i]=st.peek();
-            }
-            st.push(i);
-        }
-        return ans;
-
-    }
-    private int[] pse(int[] heights){
-        int n=heights.length;
-        Stack<Integer> st=new Stack();
+        Stack<Integer> st=new Stack<>();
         int[] ans=new int[n];
         for(int i=0;i<n;i++){
             while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
@@ -35,19 +16,38 @@ class Solution {
             st.push(i);
         }
         return ans;
-
+    }
+    public int[] nse(int[] heights){
+        int n=heights.length;
+        Stack<Integer> st=new Stack<>();
+        int[] ans=new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i]=n;
+            }
+            else{
+                ans[i]=st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
     }
     public int largestRectangleArea(int[] heights) {
-        int[] next=nse(heights);
-        int[] pre=pse(heights);
         int n=heights.length;
+        int[] nexts=nse(heights);
+        int[] pres=pse(heights);
         int maxarea=0;
+        int area=0;
         for(int i=0;i<n;i++){
-            int width=next[i]-pre[i]-1;
-            int area=heights[i]*width;
-            maxarea=Math.max(maxarea,area);
+            int right=nexts[i]-i;
+            int left=i-pres[i];
+            int width=(left+right)-1;
+            area=heights[i]*width;
+            maxarea=Math.max(area,maxarea);
         }
         return maxarea;
-
     }
 }
