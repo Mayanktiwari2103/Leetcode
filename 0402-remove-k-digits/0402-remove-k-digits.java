bcd6c55ba12f8dc90775ffee5240940946c7cd9f
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-02-06 17:29:58 +0530

    Time: 27 ms (32.28%), Space: 48.2 MB (16.4%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,34 +1,27 @@
 class Solution {
-   
     public String removeKdigits(String num, int k) {
-        int n=num.length();
-        Stack<Character> st=new Stack();
-        StringBuilder res=new StringBuilder();
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty() && k>0 && (st.peek()-'0')>(num.charAt(i)-'0')){
+        int n = num.length();
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && k > 0 && st.peek() >num.charAt(i)) {
                 st.pop();
-                k=k-1;
+                k--;
             }
             st.push(num.charAt(i));
         }
-        //edge cases
-         while(k>0){// when the number is sorted
+        while (k > 0 && !st.isEmpty()) {
             st.pop();
             k--;
-         }
-         if(st.isEmpty()){
-            return "0";
-         }
-         while(!st.isEmpty()){
-            res.append(st.pop());
-         }
-         res.reverse();
-         while(res.length()!=0 && res.charAt(0)=='0'){
-            res.deleteCharAt(0);
-         }
-         return res.length()==0 ? "0" :res.toString();
-
-
-    
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        sb.reverse();
+        while(sb.length()>1 && sb.charAt(0)=='0'){
+            sb.deleteCharAt(0);
+        }
+        if(sb.length() == 0) return "0";
+        return sb.toString();
     }
 }
