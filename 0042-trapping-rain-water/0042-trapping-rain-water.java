4a6db75b17a0c6e351bd8e879ac7d52ae0625490
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-02-10 19:58:01 +0530

    Time: 1 ms (60.8%), Space: 47.7 MB (81.2%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,15 +1,15 @@
 class Solution {
-   
     public int trap(int[] height) {
         int n=height.length;
-        int leftmax=0,rightmax=0;
         int l=0;
         int r=n-1;
+        int leftmax=0;
+        int rightmax=0;
         int sum=0;
         while(l<r){
             leftmax=Math.max(leftmax,height[l]);
             rightmax=Math.max(rightmax,height[r]);
-            if(leftmax < rightmax){
+            if(leftmax<rightmax){
                 sum+=leftmax-height[l];
                 l++;
             }
@@ -17,8 +17,8 @@ class Solution {
                 sum+=rightmax-height[r];
                 r--;
             }
+
         }
         return sum;
-
     }
 }
