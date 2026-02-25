bf949369290cc005d039db6b5445e6bc4b579a64
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-02-25 12:52:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Iterative Inorder/iterative-inorder.java
@@ -0,0 +1,34 @@
+// User function Template for Java
+
+/* A Binary Tree node
+
+class Node {
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
+class Solution {
+    // Return a list containing the inorder traversal of the given tree
+    ArrayList<Integer> inOrder(Node root) {
+        // Code
+        ArrayList<Integer> ls=new ArrayList<>();
+        if(root==null) return ls;
+        Stack<Node> st=new Stack<Node>();
+        Node curr=root;
+        while(curr!=null || !st.isEmpty()){
+            while(curr!=null){
+                st.push(curr);
+                curr=curr.left;
+            }
+            curr=st.pop();
+            ls.add(curr.data);
+            curr=curr.right;
+            
+        }
+        return ls;
+        
+    }
+}
