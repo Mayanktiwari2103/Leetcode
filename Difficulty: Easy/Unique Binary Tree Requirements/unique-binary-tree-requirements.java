952fc041ccb3e5c8010be20c836cd6dbb652d871
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-03-18 18:23:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Unique Binary Tree Requirements/unique-binary-tree-requirements.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Solution {
+
+    public static boolean isPossible(int a, int b) {
+        // Code here
+        if(a==2 && b==1 || a==2 && b==3 || a==1 && b==2 || a==3 && b==2) return true;
+        else{
+             return false;
+        }
+    }
+    
+}
