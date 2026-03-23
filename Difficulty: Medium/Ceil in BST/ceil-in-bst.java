301bf6e39f149356e99693d36e1413959af6ca2e
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-03-23 16:22:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ceil in BST/ceil-in-bst.java
@@ -0,0 +1,26 @@
+/* class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    int findCeil(Node root, int x) {
+        // code here
+        int ceil=-1;
+        while(root!=null){
+            if(x > root.data){
+               root=root.right;
+            }
+            else{
+               ceil=root.data;
+               root=root.left;
+            }
+        }
+        return ceil;
+    }
+}
