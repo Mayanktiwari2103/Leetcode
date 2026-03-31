c872a8d64587f1d570780e6eaf2c9fb310bb2485
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-03-31 15:37:25 +0530

    Time: 15 ms (12.15%), Space: 50 MB (82.38%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -3,33 +3,31 @@ class Solution {
         int n=weights.length;
         int l=Arrays.stream(weights).max().getAsInt();
         int r=Arrays.stream(weights).sum();
+        int ans=0;
         while(l<=r){
             int mid=l+(r-l)/2;
             if(capacity(weights,mid)<=days){
+                ans=mid;
                 r=mid-1;
             }
             else{
                 l=mid+1;
             }
-
         }
-        return l;
+        return ans;
+
+
     }
-    public int capacity(int[] weights,int k){
+    public int capacity(int[] weights, int cap){
         int n=weights.length;
         int sum=0;
         int day=1;
         for(int i=0;i<n;i++){
-            if(weights[i]+sum>k){
-                day++;
+            sum+=weights[i];
+            if(sum > cap){
                 sum=weights[i];
+                day++;
             }
-            else{
-                sum+=weights[i];
-            }
-            
-
-
         }
         return day;
     }
