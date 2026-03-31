59b56a9712d96611380d59d2c497c35cccd52097
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-03-31 07:38:39 +0530

    Time: 28 ms (9.67%), Space: 47.8 MB (80.67%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,28 +1,29 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int n = piles.length;
-        int l = 1;
-        int r = Arrays.stream(piles).max().getAsInt();
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (findk(piles, mid) <= h) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+        int n=piles.length;
+        int l=1;
+        int r=Arrays.stream(piles).max().getAsInt();
+        int ans=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(findk(piles,mid)<=h){
+                ans=mid;
+                r=mid-1;
             }
-
+            else{
+                l=mid+1;
+            }
         }
-        return l;
+        return ans;
+       
     }
-
-    public int findk(int[] piles, int k) {
-        int n = piles.length;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += Math.ceil(piles[i] / (double)k);
+    public int findk(int[] piles, int k){
+        int sum=0;
+        int n=piles.length;
+        for(int i=0;i<n;i++){
+            sum+=Math.ceil((double) piles[i]/k);
 
         }
-
         return sum;
 
     }
