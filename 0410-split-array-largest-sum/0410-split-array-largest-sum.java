2b132be2e0f140767d73945e943a6c7bb3ae4c36
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-03-31 18:46:32 +0530

    Time: 3 ms (14.17%), Space: 43.2 MB (42.7%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -3,30 +3,33 @@ class Solution {
         int n=nums.length;
         int l=Arrays.stream(nums).max().getAsInt();
         int r=Arrays.stream(nums).sum();
+        int ans=0;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(subarray(nums,mid)<=k){
+            if(subarrays(nums,mid) <= k){
+                ans=mid;
                 r=mid-1;
             }
             else{
                 l=mid+1;
+
             }
         }
-        return l;
+        return ans;
     }
-    public int subarray(int[] nums,int a){
+
+    private int subarrays(int[] nums, int sum){
         int n=nums.length;
-        int sum=0;
-        int cnt=1;
+        int cntsubarray=1;
+        int total=0;
         for(int i=0;i<n;i++){
-            if(sum+nums[i]>a){
-                cnt++;
-                sum=nums[i];
+            total+=nums[i];
+            if(total > sum){
+                cntsubarray++;
+                total=nums[i];
             }
-            else{
-                sum+=nums[i];
-            }
+
         }
-        return cnt;
+        return cntsubarray;
     }
 }
