026159c9249b96df7d90c250dbc25f4b7265fff0
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-04-10 15:15:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -2,38 +2,49 @@ class Solution {
     public boolean isCyclic(int V, int[][] edges) {
         // code here
         ArrayList<ArrayList<Integer>> adj=new ArrayList<ArrayList<Integer>>();
+        
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
+        
         for(int[] edge:edges){
-            int u=edge[0];
-            int v=edge[1];
-            adj.get(u).add(v);
+            adj.get(edge[0]).add(edge[1]);
         }
-        int[] visited=new int[V];
-        for(int i=0;i<V;i++){
-            if(visited[i]==0){
-                if(dfsdetect(i,adj,visited)==true){
-                    return true;
-                }
-            }
-        }
-        return false;
+        
+        return possible(V,adj);
+        
     }
     
-    private boolean dfsdetect(int start,ArrayList<ArrayList<Integer>> adj,int[] visited ){
-        visited[start]=1;
-        for(int it:adj.get(start)){
-            if(visited[it]==0){
-                if(dfsdetect(it,adj,visited)){
-                    return true;
-                }
-            }
-            else if(visited[it]==1){
-                return true;
+    private boolean possible(int V,ArrayList<ArrayList<Integer>> adj){
+        
+        int[] indegree=new int[V];
+        for(int i=0;i<V;i++){
+            for(int it:adj.get(i)){
+                indegree[it]++;
             }
         }
-        visited[start]=2;
-        return false;
+        
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        int cnt=0;
+        while(!q.isEmpty()){
+            int node=q.poll();
+            cnt++;
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.add(it);
+                }
+            }
+        }
+        
+        if(cnt==V){
+            return false;
+        }
+        return true;
     }
 }
