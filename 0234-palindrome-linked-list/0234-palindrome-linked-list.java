7088ffc1c7689fa8c622068b1987d7b9e7750aae
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-05-29 14:45:28 +0530

    Time: 3 ms (99.82%), Space: 94.3 MB (77.91%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,41 +9,34 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode temp = curr.next;
-        while (temp != null) {
-            curr.next = prev;
-            prev = curr;
-            curr = temp;
-            temp = temp.next;
-
-        }
-        curr.next=prev;
-        prev=curr;
-        return prev;
-    }
-
     public boolean isPalindrome(ListNode head) {
-        if (head == null)
-            return false;
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast!= null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        if(head==null) return false;
+
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        if(fast!=null) slow=slow.next;
-        ListNode newhead = reverse(slow);
-        while(newhead!=null){
-            if(head.val!=newhead.val) return false;
-            head=head.next;
-            newhead=newhead.next;
+        ListNode prev=null;
+        ListNode temp=slow;
+        while(temp!=null){
+            ListNode front=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=front;
+        }
+
+        ListNode first=head;
+        ListNode second=prev;
+
+        while(second!=null){
+            if(first.val!=second.val){
+                return false;
+            }
+            first=first.next;
+            second=second.next;
         }
         return true;
-
     }
 }
