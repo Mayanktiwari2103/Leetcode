ef82b23c2795d7d0957749e7e493f71ad0b20c87
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-05-29 15:59:44 +0530

    Time: 11 ms (34.61%), Space: 59.6 MB (31.65%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,7 +9,8 @@
  * }
  */
 class Solution {
-    public ListNode merge(ListNode l1,ListNode l2){
+
+    public ListNode merge(ListNode l1 , ListNode l2){
         ListNode dummy=new ListNode(0);
         ListNode temp=dummy;
         while(l1!=null && l2!=null){
@@ -24,34 +25,33 @@ class Solution {
                 temp=temp.next;
             }
         }
+
         while(l1!=null){
             temp.next=l1;
             l1=l1.next;
             temp=temp.next;
         }
+
         while(l2!=null){
             temp.next=l2;
             l2=l2.next;
             temp=temp.next;
         }
         return dummy.next;
-
-
     }
+    
     public ListNode sortList(ListNode head) {
         if(head==null || head.next==null) return head;
         ListNode slow=head;
         ListNode fast=head;
-        while(fast.next!=null  && fast.next.next!=null ){
+        while(fast.next!=null && fast.next.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-
         }
-        ListNode rightstart=slow.next;
+        ListNode righthalf=slow.next;
         slow.next=null;
         ListNode left=sortList(head);
-        ListNode right=sortList(rightstart);
+        ListNode right=sortList(righthalf);
         return merge(left,right);
-
     }
 }
