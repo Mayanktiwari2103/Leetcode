0336eacc62b6bed6c782052db48ba4a7dcfc3048
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-05-30 12:57:08 +0530

    Time: 0 ms (100%), Space: 44 MB (93.92%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -9,7 +9,7 @@
  * }
  */
 class Solution {
-    public ListNode find(ListNode head,int k){
+    public ListNode findk(ListNode head, int k){
         int cnt=1;
         ListNode temp=head;
         while(temp!=null){
@@ -21,24 +21,19 @@ class Solution {
     }
     public ListNode rotateRight(ListNode head, int k) {
         if(head==null || head.next==null) return head;
-       ListNode temp=head;
-       int cnt=1;
-       while(temp.next!=null){
+      ListNode temp=head;
+      int len=1;
+      while(temp.next!=null){
+        len++;
         temp=temp.next;
-        cnt++;
-       }
-       ListNode tail=temp;
-       int len=cnt;
-       if(k%len==0) return head;
-       k=k%len;
-       tail.next=head;
-       ListNode newtail=find(head,len-k);
-       head=newtail.next;
-       newtail.next=null;
-       return head;
-
-        
-        
-
+      }
+      if(k%len==0) return head;
+      k=k%len;
+      ListNode tail=temp;
+      tail.next=head;
+      ListNode newtail=findk(head,len-k);
+      head=newtail.next;
+      newtail.next=null;
+      return head;
     }
 }
