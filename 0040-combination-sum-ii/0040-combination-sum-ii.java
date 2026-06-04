433bf31d1bf0348ff9f8e7c979b1c845e43f24c3
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-06-04 18:25:59 +0530

    Time: 7 ms (35.99%), Space: 45.2 MB (50.92%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<List<Integer>> result=new ArrayList<>();
-        generate(candidates,target,result,new ArrayList<>(),0,0);
-        return result;
+       List<List<Integer>> ls=new ArrayList<>();
+       generate(candidates, target , ls, new ArrayList<>(), 0,0);
+       return ls;
     }
-    public void generate(int[] candidates, int target, List<List<Integer>> result,List<Integer> list,int sum,int start){
+
+    private void generate(int[] candidates, int target,List<List<Integer>> ls, List<Integer> list, int sum , int ind ){
+        if(sum> target) return;
         if(sum==target){
-            result.add(new ArrayList<>(list));
-        }
-        if(sum>target){
+            ls.add(new ArrayList<>(list));
             return;
         }
-        for(int i=start;i<candidates.length;i++){
-            if(i>start && candidates[i]==candidates[i-1]) continue;
+
+        for(int i=ind;i<candidates.length;i++){
+            if(i>ind && candidates[i]==candidates[i-1]) continue;
             list.add(candidates[i]);
-            generate(candidates,target,result,list,sum+candidates[i],i+1);
+            generate(candidates,target, ls, list,sum+candidates[i],i+1);
             list.remove(list.size()-1);
         }
     }
+    
 }
