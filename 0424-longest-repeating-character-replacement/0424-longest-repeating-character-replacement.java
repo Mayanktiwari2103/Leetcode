3c5c06dc121902bbf039e944f8571d5e5c585e5d
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-06-14 03:42:05 +0530

    Time: 10 ms (57.74%), Space: 46.2 MB (64.48%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -3,20 +3,18 @@ class Solution {
         int n=s.length();
         int l=0;
         int r=0;
+        int freq[]=new int[26];
         int maxlen=0;
         int maxfreq=0;
-        int freq[]=new int[26];
         while(r<n){
             freq[s.charAt(r)-'A']++;
             maxfreq=Math.max(maxfreq,freq[s.charAt(r)-'A']);
-            int changes=(r-l+1)-maxfreq;
-            if(changes>k){
-                freq[s.charAt(l)-'A']--;
-                l++;
+            int change=(r-l+1)-maxfreq;
+            if(change > k){
+               freq[s.charAt(l)-'A']--;
+               l++;
             }
-            if(changes<=k){
-                maxlen=Math.max(maxlen,r-l+1);
-            }
+            if(change <=k) maxlen=Math.max(maxlen,r-l+1);
             r++;
         }
         return maxlen;
