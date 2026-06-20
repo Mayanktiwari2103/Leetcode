5fafbda986183970abca5e4e457cbedaab9c6d8a
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-06-20 22:34:41 +0530

    Time: 0 ms (100%), Space: 42.6 MB (92.87%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,13 +15,16 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null) return true;
-        if(p==null || q==null) return false;
-        if(p.val!=q.val){
-            return false;
-
-        }
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+       return check(p,q);
     }
-    
+    private boolean check(TreeNode p, TreeNode q){
+        if(p==null && q==null) return true;
+        if(p!=null && q==null) return false;
+        if(p==null && q!=null) return false;
+        if(p.val!=q.val) return false;
+        boolean left=check(p.left,q.left);
+        boolean right=check(p.right,q.right);
+        return left && right;
+
+    }
 }
