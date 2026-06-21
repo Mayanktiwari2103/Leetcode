f998d5cee9f5dc6bc62146ec12f4fe715d20fb67
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-06-22 03:09:06 +0530

    Time: 4 ms (59.84%), Space: 44.1 MB (50.61%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -13,56 +13,52 @@
  *     }
  * }
  */
-class Triplet {
+class Tuple{
     TreeNode node;
     int row;
     int col;
-
-    Triplet(TreeNode node, int row, int col) {
-        this.node = node;
-        this.row = row;
-        this.col = col;
+    Tuple(TreeNode node,int row, int col){
+        this.node=node;
+        this.row=row;
+        this.col=col;
     }
 }
-
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>> result=new ArrayList<>();
-        if(root==null) return result;
-        Queue<Triplet> q=new LinkedList<>();
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map=new TreeMap<>();
-        q.offer(new Triplet(root,0,0));
-        while(!q.isEmpty()){
-            Triplet p=q.poll();
-            TreeNode node=p.node;
-            int row=p.row;
-            int col=p.col;
-            map
-                .computeIfAbsent(col,k->new TreeMap<>())
-                .computeIfAbsent(row,k->new PriorityQueue<>())
-                .offer(node.val);
-            if(node.left!=null){
-                q.offer(new Triplet(node.left,row+1,col-1));
-            } 
-            if(node.right!=null){
-                q.offer(new Triplet(node.right,row+1,col+1));
+        List<List<Integer>> ls=new ArrayList<>();
+       Queue<Tuple> q=new LinkedList<>();
+       TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map=new TreeMap<>();
+       q.add(new Tuple(root,0,0));
+       while(!q.isEmpty()){
+        Tuple p=q.poll();
+        TreeNode curr=p.node;
+        int rows=p.row;
+        int cols=p.col;
+        if(!map.containsKey(cols)){
+            map.put(cols,new TreeMap<>());
+        }
+        if(!map.get(cols).containsKey(rows)){
+            map.get(cols).put(rows,new PriorityQueue<>());
+        }
+
+        map.get(cols).get(rows).add(curr.val);
+        if(curr.left!=null){
+            q.add(new Tuple(curr.left,rows+1,cols-1));
+        }
+        if(curr.right!=null){
+            q.add(new Tuple(curr.right,rows+1,cols+1));
+        }
+       }
+
+       for(TreeMap<Integer,PriorityQueue<Integer>> rows:map.values()){
+        List<Integer> list=new ArrayList<>();
+        for(PriorityQueue<Integer> pq:rows.values()){
+            while(!pq.isEmpty()){
+                list.add(pq.poll());
             }
         }
-
-       
-for (TreeMap<Integer, PriorityQueue<Integer>> rows : map.values()) {
-
-    List<Integer> columnList = new ArrayList<>();
-
-    for (PriorityQueue<Integer> pq : rows.values()) {
-
-        while (!pq.isEmpty()) {
-            columnList.add(pq.poll());
-        }
-    }
-
-    result.add(columnList);
-}
-        return result;
+        ls.add(list);
+       }
+       return ls;
     }
 }
