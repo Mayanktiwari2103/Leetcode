aba98841902e89300af4cb9b1ee1137bf5956963
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-06-26 19:40:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,53 +1,51 @@
 class Pair{
-    int node;
-      int parent;
-      Pair( int node,int parent){
-        this.node=node;
-        this.parent=parent;
-      }  
+    int first;
+    int second;
+    Pair(int first,int second){
+        this.first=first;
+        this.second=second;
+    }
 }
 class Solution {
     public boolean isCycle(int V, int[][] edges) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-for (int i = 0; i < V; i++) {
-    adj.add(new ArrayList<>());
-}
-
-for (int[] edge : edges) {
-    int u = edge[0];
-    int v = edge[1];
-
-    adj.get(u).add(v);
-    adj.get(v).add(u);
-}
-        int visited[]=new int[V];
+        ArrayList<ArrayList<Integer>> ls=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            ls.add(new ArrayList<>());
+        }
+        for(int[] edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            ls.get(u).add(v);
+            ls.get(v).add(u);
+        }
+        int[] visited=new int[V];
         for(int i=0;i<V;i++){
             if(visited[i]==0){
-                if(detectcycle(i,adj,visited)==true){
+                if(detect(V,ls,i,visited)==true){
                     return true;
                 }
             }
         }
         return false;
+        
     }
-    private boolean detectcycle(int start, ArrayList<ArrayList<Integer>> adj ,int visited[]){
+    
+    private boolean detect(int V,ArrayList<ArrayList<Integer>> ls,int src,int[] visited){
         Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(start,-1));
-        visited[start]=1;
+        q.add(new Pair(src,-1));
+        visited[src]=1;
         while(!q.isEmpty()){
-            int node=q.peek().node;
-            int parent=q.peek().parent;
+            int node=q.peek().first;
+            int parent=q.peek().second;
             q.poll();
-            for(int it: adj.get(node)){
-                if(visited[it]==0){
-                    visited[it]=1;
-                    q.add(new Pair(it,node));
+            for(int adjnode:ls.get(node)){
+                if(visited[adjnode]==0){
+                    q.add(new Pair(adjnode,node));
+                    visited[adjnode]=1;
                 }
-                else if(parent!=it){
+                else if(adjnode!=parent){
                     return true;
                 }
-
             }
         }
         return false;
