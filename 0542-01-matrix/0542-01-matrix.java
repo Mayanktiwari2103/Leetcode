ecc1c347ce1bb4cd5ca535207e25f27270760691
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-06-27 22:04:38 +0530

    Time: 17 ms (18.28%), Space: 50 MB (5.25%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,7 +2,7 @@ class Pair{
     int row;
     int col;
     int dist;
-    Pair(int row, int col , int dist){
+    Pair(int row,int col,int dist){
         this.row=row;
         this.col=col;
         this.dist=dist;
@@ -12,37 +12,36 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n=mat.length;
         int m=mat[0].length;
-        int[][] visited=new int[n][m];
+        int[][] vis=new int[n][m];
         int[][] distance=new int[n][m];
         Queue<Pair> q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
                     q.add(new Pair(i,j,0));
-                    visited[i][j]=1;
+                    vis[i][j]=1;
                 }
             }
-        }
+        }  
+
         int[] drow={-1,0,1,0};
         int[] dcol={0,1,0,-1};
         while(!q.isEmpty()){
             int row=q.peek().row;
             int col=q.peek().col;
-            int d=q.peek().dist;
-            distance[row][col]=d;
+            int dist=q.peek().dist;
             q.poll();
             for(int i=0;i<4;i++){
                 int nrow=row+drow[i];
                 int ncol=col+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]==0 && mat[nrow][ncol]==1){
-                    q.add(new Pair(nrow,ncol,d+1));
-                    visited[nrow][ncol]=1;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && mat[nrow][ncol]==1 && vis[nrow][ncol]==0){
+                    q.add(new Pair(nrow,ncol,dist+1));
+                    vis[nrow][ncol]=1;
+                    distance[nrow][ncol]=dist+1;
                 }
             }
-        }
-        return distance;
-        
+        }  
 
-        
+        return distance;   
     }
 }
