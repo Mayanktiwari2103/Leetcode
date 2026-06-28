64ece12d21534c2403b9bf165b41009f02e24307
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-06-29 00:19:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.java
+++ Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.java
@@ -1,40 +1,47 @@
+class Pair{
+    int node;
+    int dist;
+    Pair(int node, int dist){
+        this.node=node;
+        this.dist=dist;
+    }
+}
 class Solution {
     
     public int[] shortestPath(int V, int[][] edges, int src) {
         // code here
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<ArrayList<Integer>>();
-        int[] dist=new int[V];
-        Arrays.fill(dist,Integer.MAX_VALUE);
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        
         for(int[] edge:edges){
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
+            int u=edge[0];
+            int v=edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-        
-        dist[src]=0;
-        Queue<Integer> q=new LinkedList<>();
-        q.add(src);
-        
+        int[] distance=new int[V];
+        Arrays.fill(distance,Integer.MAX_VALUE);
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(src,0));
+        distance[src]=0;
         while(!q.isEmpty()){
-            int node=q.poll();
+            int node=q.peek().node;
+            int dist=q.peek().dist;
+            q.poll();
             for(int it:adj.get(node)){
-                if(dist[node] +1 < dist[it]){
-                    dist[it]=dist[node]+1;
-                    q.add(it);
+                if(dist+1 < distance[it]){
+                    distance[it]=dist+1;
+                    q.add(new Pair(it,distance[it]));
                 }
             }
         }
-        
         for(int i=0;i<V;i++){
-            if(dist[i]==Integer.MAX_VALUE){
-                dist[i]=-1;
+            if(distance[i]==Integer.MAX_VALUE){
+                distance[i]=-1;
             }
         }
-        return dist;
-        
+        return distance;
     }
     
     
