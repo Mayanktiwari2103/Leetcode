ccc0b899c06cf9212b3fa39741fa29c064c71722
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-06-28 18:10:59 +0530

    Time: 28 ms (22.55%), Space: 60.3 MB (88.72%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,38 +1,40 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        ArrayList<ArrayList<Integer>> revadj=new ArrayList<ArrayList<Integer>>();
-        int V=graph.length;
-        int[] indegree=new int[V];
-        for(int i=0;i<V;i++){
-           revadj.add(new ArrayList<>());
+       int n=graph.length;
+       List<Integer> ls=new ArrayList<>();
+       ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+       for(int i=0;i<n;i++){
+        adj.add(new ArrayList<>());
+       }
+       int[] outdegree=new int[n];
+       for(int i=0;i<n;i++){
+        for(int j=0;j<graph[i].length;j++){
+            int neighbour=graph[i][j];
+            adj.get(neighbour).add(i);
+            outdegree[i]++;
         }
-        for (int i = 0; i < V; i++) {
-          for (int j = 0; j < graph[i].length; j++) {
-             int neighbor = graph[i][j];
-              revadj.get(neighbor).add(i); 
-              indegree[i]++;
-            }
-        }
-        Queue<Integer> q=new LinkedList<>();
-        List<Integer> safe=new ArrayList<Integer>();
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0){
-                q.add(i);
-            }
-        }
-        while(!q.isEmpty()){
-            int node=q.peek();
-            safe.add(q.poll());
-            for(int it:revadj.get(node)){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.add(it);
-                }
-            }
-        }
+       }
 
-        Collections.sort(safe);
-        return safe;
+       Queue<Integer> q=new LinkedList<>();
+       for(int i=0;i<n;i++){
+        if(outdegree[i]==0){
+            q.add(i);
+        }
+       }
+
+       while(!q.isEmpty()){
+        int node=q.poll();
+        ls.add(node);
+        for(int it:adj.get(node)){
+            outdegree[it]--;
+            if(outdegree[it]==0){
+                q.add(it);
+            }
+        }
+       }   
+
+       Collections.sort(ls);
+       return ls;
 
     }
 }
