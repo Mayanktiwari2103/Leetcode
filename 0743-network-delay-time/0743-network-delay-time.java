1392672c80d61a8f0e0b14a34281a4d6b2f12789
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-06-30 01:01:30 +0530

    Time: 10 ms (83.22%), Space: 49.1 MB (50.08%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -8,46 +8,44 @@ class Pair{
 }
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        ArrayList<ArrayList<Pair>> adj=new ArrayList<ArrayList<Pair>>();
-        for(int i=0;i<=n;i++){
-            adj.add(new ArrayList<>());
-        }
+       ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
+       for(int i=0;i<=n;i++){
+        adj.add(new ArrayList<>());
+       } 
 
-        for(int[] edge:times){
-            int u=edge[0];
-            int v=edge[1];
-            int w=edge[2];
-            adj.get(u).add(new Pair(v,w));
-        }
-
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)-> a.first-b.first);
-        int[] distance=new int[n+1];
-        for(int i=1;i<=n;i++){
-            distance[i]=Integer.MAX_VALUE;
-        }
-        pq.add(new Pair(0,k));
-        distance[k]=0;
-        while(!pq.isEmpty()){
-            int time=pq.peek().first;
-            int node=pq.peek().second;
-            pq.poll();
-            for(Pair it:adj.get(node)){
-                int adjnode=it.first;
-                int t=it.second;
-                if(t+time < distance[adjnode]){
-                    pq.add(new Pair(t+time,adjnode));
-                    distance[adjnode]=t+time;
-                }
+       for(int[] time:times){
+        int u=time[0];
+        int v=time[1];
+        int w=time[2];
+        adj.get(u).add(new Pair(v,w));
+       }
+       int[]distance=new int[n+1];
+       for(int i=1;i<=n;i++){
+        distance[i]=Integer.MAX_VALUE;
+       }
+       PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)-> a.first-b.first);
+       pq.add(new Pair(0,k));
+       distance[k]=0;
+       while(!pq.isEmpty()){
+        int first=pq.peek().first;
+        int second=pq.peek().second;
+        pq.poll();
+        for(Pair it:adj.get(second)){
+            int u=it.first;
+            int v=it.second;
+            if(first+v < distance[u]){
+                distance[u]=first+v;
+                pq.add(new Pair(distance[u],u));
             }
         }
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            if(distance[i]==Integer.MAX_VALUE){
-                return -1;
-            }
-            ans=Math.max(ans,distance[i]);
+       }
+       int max=0;
+       for(int i=1;i<=n;i++){
+        if(distance[i]==Integer.MAX_VALUE){
+            return -1;
         }
-        return ans;
-        
+        max=Math.max(max,distance[i]);
+       }
+       return max;
     }
 }
