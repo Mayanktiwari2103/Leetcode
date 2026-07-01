765f3c8df96ca326ab5b751d60accdae6a236d27
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-07-01 19:42:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree - Kruskal's Algorithm/minimum-spanning-tree-kruskals-algorithm.java
+++ Difficulty: Medium/Minimum Spanning Tree - Kruskal's Algorithm/minimum-spanning-tree-kruskals-algorithm.java
@@ -1,34 +1,33 @@
-// User function Template for Java
-class DisjointSet {
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    public DisjointSet(int n) {
-        for (int i = 0; i <= n; i++) {
-            parent.add(i);
+class Disjoint{
+    List<Integer> parent=new ArrayList<>();
+    List<Integer> size=new ArrayList<>();
+    Disjoint(int n){
+        for(int i=0;i<=n;i++){
             size.add(1);
+            parent.add(i);
         }
     }
-
-    public int findUPar(int node) {
-        if (node == parent.get(node)) {
+    
+    public int findUpar(int node){
+        if(node==parent.get(node)){
             return node;
         }
-        int ulp = findUPar(parent.get(node));
-        parent.set(node, ulp);
+        int ulp=findUpar(parent.get(node));
+        parent.set(node,ulp);
         return parent.get(node);
     }
-
-
-    public void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size.get(ulp_u) < size.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
-        } else {
-            parent.set(ulp_v, ulp_u);
-            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+    
+    public void unionbysize(int u, int v){
+        int ulp_u=findUpar(u);
+        int ulp_v=findUpar(v);
+        if(ulp_u==ulp_v) return;
+        if(size.get(ulp_u) < size.get(ulp_v)){
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
+        }
+        else{
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
         }
     }
 }
@@ -36,19 +35,17 @@ class Solution {
     static int kruskalsMST(int V, int[][] edges) {
         // code here
         Arrays.sort(edges,(a,b)-> a[2]-b[2]);
-        DisjointSet ds=new DisjointSet(V);
-        int mstwt=0;
+        Disjoint ds=new Disjoint(V);
+        int sum=0;
         for(int[] edge:edges){
             int u=edge[0];
             int v=edge[1];
             int w=edge[2];
-            
-            if(ds.findUPar(u)!=ds.findUPar(v)){
-                mstwt+=w;
-                ds.unionBySize(u,v);
+            if(ds.findUpar(u)!=ds.findUpar(v)){
+                ds.unionbysize(u,v);
+                sum+=w;
             }
         }
-        return mstwt;
+        return sum;
     }
-    
 }
