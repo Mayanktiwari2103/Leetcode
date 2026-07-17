02106b933d0e39647f97e4f0ed3cf0e0e7f07833
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-07-17 20:01:42 +0530

    Time: 3 ms (98.38%), Space: 47.2 MB (34.86%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,21 @@
+class Solution {
+    private int generate(String word1 , String word2,int i , int j, int[][] dp){
+        if(i < 0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(word1.charAt(i)==word2.charAt(j)){
+            return dp[i][j]= generate(word1,word2,i-1,j-1,dp);
+        }
+        return dp[i][j]= 1+Math.min(generate(word1, word2 , i,j-1,dp),Math.min(generate(word1, word2 , i-1 , j,dp), generate(word1 , word2 , i-1, j-1 , dp)));
+    }
+    public int minDistance(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+
+        return generate(word1, word2 , n-1 , m-1,dp);
+    }
+}
