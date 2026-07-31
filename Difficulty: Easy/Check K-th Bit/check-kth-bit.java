f408eb136b3a37aabf7d54452ea18dacdd3f71d1
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-07-31 18:51:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check K-th Bit/check-kth-bit.java
+++ Difficulty: Easy/Check K-th Bit/check-kth-bit.java
@@ -1,15 +1,8 @@
 class CheckBit {
     static boolean checkKthBit(int n, int k) {
         // code here
-        StringBuilder sb=new StringBuilder();
-        while(n!=0){
-            sb.append(n%2);
-            n=n/2;
-        }
-        sb=sb.reverse();
-        int m=sb.length();
-        if(m <= k) return false;
-        if(sb.charAt(m-1-k)=='1') return true;
+        n=n >> k;
+        if((n&1)!=0) return true;
         return false;
     }
 }
