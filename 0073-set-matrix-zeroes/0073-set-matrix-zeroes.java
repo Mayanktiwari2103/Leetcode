6acbae2e089f12b2e6e920361508e1285e87a782
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-05 18:13:46 +0530

    Time: 1 ms (97.35%), Space: 47.5 MB (81.52%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -12,13 +12,13 @@ class Solution {
                 }
             }
         }
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(row[i]==1 || col[j]==1){
-                   matrix[i][j]=0;
+                    matrix[i][j]=0;
                 }
             }
         }
-
     }
 }
