63682b2162c161348b489a6cf9031f37ce93207d
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-06 12:29:09 +0530

    Time: 31 ms (5.56%), Space: 82.3 MB (92.49%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,38 +1,34 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int n=bloomDay.length;
-        if(n<(long)m*k) return -1;
-        int l=Arrays.stream(bloomDay).min().getAsInt();
-        int r=Arrays.stream(bloomDay).max().getAsInt();
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(bouquets(bloomDay,mid,k)<m){
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
-            }
+        int n = bloomDay.length;
+        int l = Arrays.stream(bloomDay).min().getAsInt();
+        int r = Arrays.stream(bloomDay).max().getAsInt();
+        if ((long) m * k > n)
+            return -1;
 
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (possible(bloomDay, m, k, mid) < m) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
         return l;
     }
-    public int bouquets(int[] bloomDay,int a,int k){
-        int n=bloomDay.length;
-        int cnt=0;
-        int bouquet=0;
-        for(int i=0;i<n;i++){
-            if(bloomDay[i]<=a){
-                cnt++;
 
-            }
+    private int possible(int[] bloomDay, int m, int k, int day) {
+        int n = bloomDay.length;
+        int bouquet = 0;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if(bloomDay[i] <= day) cnt++;
             if(cnt==k){
-                bouquet++;
-                cnt=0;
+              bouquet++;
+              cnt=0;
             }
-            if(bloomDay[i]>a){
-                cnt=0;
-            }
-            
+            if(bloomDay[i] > day) cnt=0;
+
         }
         return bouquet;
     }
