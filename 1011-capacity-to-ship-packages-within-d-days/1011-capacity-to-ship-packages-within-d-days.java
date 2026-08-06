4f3145c1360e4ef8c1de1ae9ff1235673aa04e9d
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-06 12:49:51 +0530

    Time: 15 ms (10.16%), Space: 50.1 MB (58.34%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,33 +1,32 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int n=weights.length;
-        int l=Arrays.stream(weights).max().getAsInt();
-        int r=Arrays.stream(weights).sum();
-        int ans=0;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(capacity(weights,mid)<=days){
-                ans=mid;
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
+       int n=weights.length;
+       int l=Arrays.stream(weights).max().getAsInt();
+       int r=Arrays.stream(weights).sum();
+       int ans=0;
+       while(l<=r){
+        int mid=l+(r-l)/2;
+        if(possible(weights,days, mid) <= days){
+            ans=mid;
+            r=mid-1;
         }
-        return ans;
-
-
+        else{
+            l=mid+1;
+        }
+       }
+       return ans;
     }
-    public int capacity(int[] weights, int cap){
+
+    private int possible(int[] weights, int days, int cap){
         int n=weights.length;
-        int sum=0;
         int day=1;
+        int sum=0;
         for(int i=0;i<n;i++){
-            sum+=weights[i];
-            if(sum > cap){
-                sum=weights[i];
-                day++;
-            }
+           sum+=weights[i];
+           if(sum > cap){
+            day++;
+            sum=weights[i];
+           }
         }
         return day;
     }
