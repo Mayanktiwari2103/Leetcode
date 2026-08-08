8f6ff92a268dbccb353582ca2758f0f67f337b16
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-08 18:07:19 +0530

    Time: 16 ms (38.09%), Space: 47.7 MB (49.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n=nums.length;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<n;i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i],1);
-            }
-            else{
-                map.put(nums[i],map.get(nums[i])+1);
-            }
-        }
-        PriorityQueue<Map.Entry<Integer,Integer>> minheap=new PriorityQueue<>((a,b)->a.getValue()-b.getValue());
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            minheap.add(entry);
-            if(minheap.size()>k){
-                minheap.poll();
-            }
-        }
-        int[] result=new int[k];
-        int i=0;
-        while(!minheap.isEmpty()){
-            result[i]=minheap.poll().getKey();
-            i++;
+       int n=nums.length;
+       HashMap<Integer,Integer> map=new HashMap<>();
 
+       for(int i=0;i<n;i++){
+        if(map.containsKey(nums[i])){
+            map.put(nums[i],map.get(nums[i])+1);
         }
-        return result;
+        else{
+            map.put(nums[i],1);
+        }
+       }
 
+       PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>((a,b) -> a.getValue()-b.getValue());
+       for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+           pq.add(entry);
+           if(pq.size() > k){
+            pq.poll();
+           }
+        }
+
+        int[] ans=new int[k];
+        int ind=0;
+        while(!pq.isEmpty()){
+            ans[ind++]=pq.poll().getKey();
+        }
+        return ans;
         
     }
 }
