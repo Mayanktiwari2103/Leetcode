ed03f19828b2fdfef4d6bc1476bb8cf860c8f116
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-10 00:28:55 +0530

    Time: 12 ms (83.46%), Space: 44.4 MB (12.14%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -7,57 +7,77 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+/*class Tuple{
+    int node;
+    int parent;
+    Tuple(int node, int parent){
+        this.node=node;
+        this.parent=parent;
+    }
+}*/
 class Solution {
-    private void parentmark(TreeNode root, Map<TreeNode,TreeNode> parent){
-        Queue<TreeNode> q=new LinkedList<>();
+    private void parentmark(HashMap<TreeNode, TreeNode> map, TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        while(!q.isEmpty()){
-            TreeNode current=q.poll();
-            if(current.left!=null){
-                parent.put(current.left,current);
-                q.add(current.left);
-            }
-            if(current.right!=null){
-                parent.put(current.right,current);
-                q.add(current.right);
-            }
+        map.put(root, null);
 
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                if (curr.left != null) {
+                    q.add(curr.left);
+                    map.put(curr.left, curr);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                    map.put(curr.right, curr);
+                }
+
+            }
         }
     }
+
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        List<Integer> ls=new ArrayList<>();
-        Queue<TreeNode> q=new LinkedList<>();
-        Map<TreeNode,TreeNode> parent=new HashMap<>();
-        parentmark(root,parent);
-        Map<TreeNode,Boolean> visited=new HashMap<>();
-        int dist=0;
+        List<Integer> ls = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root == null)
+            return ls;
+        HashMap<TreeNode, TreeNode> map = new HashMap<>();
+        parentmark(map, root);
+        HashSet<TreeNode> visited = new HashSet<>();
         q.add(target);
-        visited.put(target,true);
-        while(!q.isEmpty()){
-            if(dist==k) break;
-            dist++;
-            int size=q.size();
-            for(int i=0;i<size;i++){
-               TreeNode p=q.poll();
-               if(p.left!=null && visited.get(p.left)==null){
-                 q.add(p.left);
-                 visited.put(p.left,true);
+        visited.add(target);
+        int level = 0;
 
-               }
-               if(p.right!=null && visited.get(p.right)==null){
-                  q.add(p.right);
-                  visited.put(p.right,true);
-               }
-
-               if(parent.get(p)!=null && visited.get(parent.get(p))==null){
-                q.add(parent.get(p));
-                visited.put(parent.get(p),true);
-               }
+        while (!q.isEmpty()) {
+            int size = q.size();
+            if (level == k) {
+                while (!q.isEmpty()) {
+                    ls.add(q.poll().val);
+                }
+                break;
             }
+            for (int i = 0; i < size; i++) {
+
+                TreeNode curr = q.poll();
+                if (curr.left != null && !visited.contains(curr.left)) {
+                    q.add(curr.left);
+                    visited.add(curr.left);
+                }
+                if (curr.right != null && (!visited.contains(curr.right))) {
+                    q.add(curr.right);
+                    visited.add(curr.right);
+                }
+                TreeNode parent = map.get(curr);
+                if (parent != null && (!visited.contains(parent))) {
+                    q.add(parent);
+                    visited.add(parent);
+                }
+            }
+            level++;
         }
-        while(!q.isEmpty()){
-            ls.add(q.poll().val);
-        }
+
         return ls;
 
     }
