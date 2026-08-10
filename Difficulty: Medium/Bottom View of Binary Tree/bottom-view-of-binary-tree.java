cdca714db1a64fb26fc9a4b4d6ce2e06ee8129a0
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-10 23:11:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
@@ -23,27 +23,28 @@ class Pair{
 class Solution {
     public ArrayList<Integer> bottomView(Node root) {
         // code here
-        ArrayList<Integer> ls=new ArrayList<>();
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(root,0));
-        Map<Integer,Integer> map=new TreeMap<>();
-        while(!q.isEmpty()){
-            Pair p=q.poll();
-            Node curr=p.node;
-            int col=p.col;
-            map.put(col,curr.data);
-            if(curr.left!=null){
-                q.add(new Pair(curr.left,col-1));
-            }
-            if(curr.right!=null){
-                q.add(new Pair(curr.right,col+1));
-            }
-            
-        }
-        
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            ls.add(entry.getValue());
-        }
-        return ls;
+       ArrayList<Integer> ls=new ArrayList<>();
+       TreeMap<Integer,Integer> map=new TreeMap<>();
+       Queue<Pair> q=new LinkedList<>();
+       
+       q.add(new Pair(root,0));
+       while(!q.isEmpty()){
+           Pair curr=q.poll();
+           Node node=curr.node;
+           int col=curr.col;
+           map.put(col,node.data);
+           if(node.left!=null){
+               q.add(new Pair(node.left,col-1));
+           }
+           
+           if(node.right!=null){
+               q.add(new Pair(node.right,col+1));
+           }
+       }
+       
+       for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+           ls.add(entry.getValue());
+       }
+       return ls;
     }
 }
