796745a0b3d8f1a160817360f514d6b400f75cea
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-11 11:58:12 +0530

    Time: 3 ms (9.75%), Space: 44.3 MB (24.77%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,8 +1,8 @@
-class Pair{
+class Tuple{
     int row;
     int col;
     int time;
-    Pair(int row, int col,int time){
+    Tuple(int row, int col, int time){
         this.row=row;
         this.col=col;
         this.time=time;
@@ -12,42 +12,44 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        Queue<Pair> q=new LinkedList<>();
-        int[][] visited=new int[n][m];
+        Queue<Tuple> q=new LinkedList<>();
+        int[][] vis=new int[n][m];
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                   q.add(new Pair(i,j,0));
-                   visited[i][j]=1;
+                    q.add(new Tuple(i,j,0));
+                    vis[i][j]=1;
                 }
             }
         }
+
         int[] drow={-1,0,1,0};
         int[] dcol={0,1,0,-1};
-        int time=0;
+        int second=0;
         while(!q.isEmpty()){
-            int r=q.peek().row;
-            int c=q.peek().col;
-            int t=q.peek().time;
-            time=Math.max(t,time);
+            int row=q.peek().row;
+            int col=q.peek().col;
+            int time=q.peek().time;
+            second=Math.max(second,time);
             q.poll();
             for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && visited[nrow][ncol]==0){
-                    q.add(new Pair(nrow,ncol,t+1));
-                    visited[nrow][ncol]=1;
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow >=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0 ){
+                    q.add(new Tuple(nrow,ncol,time+1));
+                    vis[nrow][ncol]=1;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1  && visited[i][j]==0){
+                if(grid[i][j]==1 && vis[i][j]==0){
                     return -1;
                 }
             }
         }
-        return time;
+        return second;
+
     }
 }
