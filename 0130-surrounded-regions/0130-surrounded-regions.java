b1031051c975b787d267628400425d558e4889bc
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-11 12:51:13 +0530

    Time: 3 ms (26.19%), Space: 47.5 MB (93.6%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,47 +1,67 @@
+class Pair{
+    int row;
+    int col;
+    Pair(int row, int col){
+        this.row=row;
+        this.col=col;
+    }
+}
 class Solution {    
     public void solve(char[][] board) {
         int n=board.length;
         int m=board[0].length;
-        int[][] visited=new int[n][m];
-        int[] drow={-1,0,1,0};
-        int[] dcol={0,1,0,-1};       
-        for(int j=0;j<m;j++){
-           if(board[0][j]=='O' && visited[0][j]==0 ){
-            dfs(0,j,board,visited,drow,dcol);
-           }
-           if(board[n-1][j]=='O' && visited[n-1][j]==0 ){
-            dfs(n-1,j,board,visited,drow,dcol);
-           }
-        }
+        int[][] vis=new int[n][m];
+        Queue<Pair> q=new LinkedList<>();
 
         for(int i=0;i<n;i++){
-            if(board[i][0]=='O' && visited[i][0]==0){
-                dfs(i,0,board,visited,drow,dcol);
+            if(board[i][0]=='O'){
+                bfs(i,0,q,vis,board);
             }
-            if(board[i][m-1]=='O' && visited[i][m-1]==0){
-                dfs(i,m-1,board,visited,drow,dcol);
+             if(board[i][m-1]=='O'){
+                bfs(i,m-1,q,vis,board);
+            }
+        }
+
+        for(int j=0;j<m;j++){
+            if(board[0][j]=='O'){
+                bfs(0,j,q,vis,board);
+            }
+             if(board[n-1][j]=='O'){
+                bfs(n-1,j,q,vis,board);
             }
         }
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(visited[i][j]==0 && board[i][j]=='O'){
+                if(board[i][j]=='O' && vis[i][j]==0){
                     board[i][j]='X';
                 }
             }
         }
+        
     }
 
-    private void dfs(int row, int col,char[][] board,int[][] visited,int[] drow, int[] dcol){
-        visited[row][col]=1;
-        int n=board.length;
+    private void bfs(int crow, int ccol, Queue<Pair> q, int[][] vis,char[][] board){
+         int n=board.length;
         int m=board[0].length;
-        for(int i=0;i<4;i++){
+        q.add(new Pair(crow,ccol));
+        vis[crow][ccol]=1;
+        int[] drow={-1,0,1,0};
+        int[] dcol={0,1,0,-1};
+        
+        while(!q.isEmpty()){
+           int row=q.peek().row;
+           int col=q.peek().col;
+           q.poll();
+           for(int i=0;i<4;i++){
             int nrow=row+drow[i];
             int ncol=col+dcol[i];
-            if(nrow>0 && nrow<n && ncol>0 && ncol<m && visited[nrow][ncol]==0 && board[nrow][ncol]=='O'){
-                dfs(nrow,ncol,board,visited,drow,dcol);
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && board[nrow][ncol]=='O' && vis[nrow][ncol]==0){
+                q.add(new Pair(nrow,ncol));
+                vis[nrow][ncol]=1;
             }
+           }
         }
+        
     }
 }
