eb7ae977c594eb012c6649aac01943a5956b20cf
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-12 11:23:11 +0530

    Time: 0 ms (100%), Space: 42.4 MB (13.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,21 @@
 class Solution {
-    private int generate(int i, int j , int[][] dp){
+    private int count(int m , int n , int i,int j , int[][] dp){
         if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int up=generate(i-1,j,dp);
-        int left=generate(i,j-1,dp);
-        return dp[i][j]=up+left;
+        int left=count(m,n,i,j-1,dp);
+        int up=count(m,n,i-1,j,dp);
+
+        return dp[i][j]=left+up;
+
     }
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
         }
-        return generate(m-1,n-1,dp);
+        return count(m,n,m-1,n-1,dp);
     }
 }
