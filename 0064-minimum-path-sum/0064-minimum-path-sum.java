daf67240279e5870a1b157425a5ae127d5047708
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-12 11:50:11 +0530

    Time: 2 ms (77.9%), Space: 50.1 MB (72.81%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,29 +1,23 @@
 class Solution {
-    private int generate(int i, int j, int[][] dp,int[][] grid){
+    private int count(int[][] grid, int i , int j , int[][] dp){
         if(i==0 && j==0) return grid[i][j];
-        if(i<0 || j<0 ) return Integer.MAX_VALUE;
+        if(i<0 || j<0) return Integer.MAX_VALUE;
         if(dp[i][j]!=-1) return dp[i][j];
-        int up=generate(i-1,j,dp,grid);
-        if(up!=Integer.MAX_VALUE){
-            up+=grid[i][j];
-        }
-        int left= generate(i,j-1,dp,grid);
-        if(left!=Integer.MAX_VALUE){
-            left+=grid[i][j];
-        }
+        int left= count(grid, i,j-1,dp);
+        int up= count(grid, i-1,j,dp);
 
-        return dp[i][j]=Math.min(up,left);
-
+        return dp[i][j]=grid[i][j] + Math.min(left,up);
     }
     public int minPathSum(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int[][] dp=new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int n=grid.length;
+        int m=grid[0].length;
+        int[][] dp=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dp[i][j]=-1;
+            }
         }
-
-        return generate(m-1,n-1,dp,grid);
+        return count(grid,n-1,m-1,dp);
 
     }
 }
