8b293c98c09a267aa08c28188c7003abce250312
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-12 09:53:41 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,15 @@
 class Solution {
+    private int count(int ind ,int[] dp){
+        if(ind<0) return 0;
+        if(ind==0) return 1;
+        if(dp[ind]!=-1) return dp[ind];
+        int one=count(ind-1,dp);
+        int two=count(ind-2,dp);
+        return dp[ind]=one+two;
+    }
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+       int[] dp=new int[n+1];
+       Arrays.fill(dp,-1);
+       return count(n,dp);
     }
 }
