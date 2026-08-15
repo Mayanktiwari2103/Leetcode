4273ac848575e182eb670ceac528d200e888cf4b
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-15 23:13:03 +0530

    Time: 29 ms (22.05%), Space: 46.9 MB (30.63%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,32 +1,31 @@
 class Solution {
-    private int generate(int[] coins , int amount,int ind,int[][] dp){
+    private int generate(int[] coins , int amount , int ind , int[][] dp){
         if(ind==0){
-            if(coins[ind]==amount){
-                return 1;
-            }
-            else if(amount % coins[ind]==0){
-                return amount/ coins[ind];
-            }
-
-            return (int)(1e9);
+           if(amount % coins[ind]==0){
+            return amount/coins[ind];
+           }
+           return (int) 1e9;
         }
         if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int not=generate(coins,amount,ind-1,dp);
-        int take=(int)(1e9);
-        if(amount>=coins[ind]){
-            take=1+generate(coins,amount-coins[ind],ind,dp);
+        int notpick=generate(coins,amount,ind-1,dp);
+        int pick=(int) 1e9;
+        if(coins[ind] <= amount){
+            pick=1+generate(coins , amount-coins[ind], ind,dp);
         }
+        
+        return dp[ind][amount]=Math.min(notpick,pick);
 
-        return dp[ind][amount]=Math.min(take,not);
     }
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                dp[i][j]=-1;
+            }
         }
         int ans=generate(coins,amount,n-1,dp);
-        return ans>=(int)(1e9)?-1:ans;
-
+        return ans>=(int) 1e9?-1:ans;
+        
     }
 }
