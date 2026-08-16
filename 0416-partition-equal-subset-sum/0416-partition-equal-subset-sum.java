9e2307bf57a66d516e83adcab436c30a077b7d61
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-16 13:13:48 +0530

    Time: 108 ms (10.11%), Space: 76 MB (13.48%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,31 +1,35 @@
-class Solution {
-    private boolean generate(int[] nums, int ind,int target,int[][] dp){
-        if(target==0){
+class Solution {    
+    private boolean generate(int[] nums , int ind , int sum , int[][] dp){
+        if(sum==0) return true;
+        if(ind==0){
+           if(nums[ind]==sum){
             return true;
+           }
+           return false;
         }
-        if(ind==0) return nums[ind]==target;
-        if(dp[ind][target]!=-1) return dp[ind][target]==1;
-        boolean not=generate(nums,ind-1,target,dp);
+        if(dp[ind][sum]!=-1) return dp[ind][sum]==1;
+        boolean not=generate(nums, ind-1,sum,dp);
         boolean take=false;
-        if(target >= nums[ind]){
-            take=generate(nums,ind-1,target-nums[ind],dp);
+        if(nums[ind] <= sum){
+            take=generate(nums, ind-1 , sum-nums[ind],dp);
         }
-        dp[ind][target]= (take || not) ?1:0 ;
-        return take || not;
-    }
+        dp[ind][sum]=not || take ?1:0;
+        return not || take;
+    }    
     public boolean canPartition(int[] nums) {
         int n=nums.length;
         int sum=0;
         for(int i=0;i<n;i++){
-           sum+=nums[i];
+            sum+=nums[i];
         }
+        if(sum%2==1) return false;
         int[][] dp=new int[n][(sum/2)+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=sum/2;j++){
+                dp[i][j]=-1;
+            }
         }
-        if(sum%2!=0) return false;
-        return generate(nums,n-1,sum/2,dp);
-        
 
+        return generate(nums , n-1, sum/2, dp);
     }
 }
