d08e5b5f60600358ffe9f6390704f9b4bae1ce53
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-08-16 18:11:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
@@ -1,43 +1,35 @@
 class Solution {
-	private int generate(int[] arr, int target, int ind, int[][] dp) {
-		if (ind == 0) {
-		    if(target==0 && arr[ind]==0){
-		        return 2;
-		    }
-			if (target == 0 || arr[ind] == target) {
-				return 1;
-			}
-			return 0;
-		}
-		if (dp[ind][target] != -1)
-			return dp[ind][target];
-		int not = generate(arr, target, ind - 1, dp);
-		int take = 0;
-		if (target >= arr[ind]) {
-			take = generate(arr, target - arr[ind], ind - 1, dp);
-		}
-		
-		return dp[ind][target] = take + not;
-		
+	
+	private int generate(int[] arr , int ind, int target , int[][] dp){
+	    if(ind==0){
+	        if(arr[ind]==0 && target==0) return 2;
+	        
+	        if(target==0 || arr[ind]==target) return 1;
+	        return 0;
+	    }
+	    if(dp[ind][target]!=-1) return dp[ind][target];
+        int not=generate(arr, ind-1, target, dp);
+        int pick=0;
+        if(arr[ind] <= target){
+            pick=generate(arr , ind-1, target-arr[ind] , dp);
+        }
+        return dp[ind][target]=not+pick;
 	}
 	public int countPartitions(int[] arr, int diff) {
 		// code here
-		int n = arr.length;
-		int sum = 0;
-		for (int i = 0; i<n; i++) {
-			sum += arr[i];
-		}
-		if (sum - diff < 0)
-			return 0;
-		if ((sum - diff) % 2 != 0)
-			return 0;
-		int target = (sum - diff)/2;
-		int[][] dp = new int[n][target + 1];
-		for (int[] row:dp) {
-			Arrays.fill(row, -1);
-		}
-		
-		return generate(arr, target, n - 1, dp);
-		
+	    int n=arr.length;
+	    int sum=0;
+	    for(int x:arr){
+	        sum+=x;
+	    }
+	    int target=(diff+sum)/2;
+	    if((diff +sum) %2 ==1) return 0; 
+	    int[][] dp=new int[n][target+1];
+        for(int[] row:dp){
+           Arrays.fill(row,-1);
+        }
+        
+        return generate(arr , n-1 , target , dp);
+	    
 	}
 }
