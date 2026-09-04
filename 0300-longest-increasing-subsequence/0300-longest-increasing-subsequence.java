d18477f994ec4f8d20687e1ff14e1ae005d645b6
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-09-04 17:06:36 +0530

    Time: 401 ms (8.09%), Space: 237.3 MB (8.19%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,22 @@
 class Solution {
-    private int generate(int[] nums, int ind , int prev , int[][] dp){
+    private int generate(int[] nums , int ind,int prev,int[][] dp){
         if(ind==nums.length) return 0;
         if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-        int len=0;
-        len=generate(nums,ind+1,prev,dp);
-        if(prev==-1 || nums[ind] > nums[prev]){
-            len=Math.max(len, 1+ generate(nums, ind+1 , ind,dp));
+        int not=generate(nums,ind+1,prev,dp);
+        int take=0;
+        if(prev==-1 || nums[prev] < nums[ind]){
+            take=1+generate(nums,ind+1, ind,dp);
         }
-        return dp[ind][prev+1]=len;
+
+        return dp[ind][prev+1]=Math.max(not,take);
     }
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
         int[][] dp=new int[n][n+1];
-        for(int[] row:dp){
+        for(int row[] : dp){
             Arrays.fill(row,-1);
         }
+
         return generate(nums,0,-1,dp);
     }
 }
