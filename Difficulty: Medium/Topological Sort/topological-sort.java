6d616729dc3979c77d1e411925b3a562b013cc07
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-09-14 00:20:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,9 +1,9 @@
 class Solution {
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
         // code here
-        ArrayList<Integer> ls=new ArrayList<>();
-        Queue<Integer> q=new LinkedList<>();
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        ArrayList<Integer> ls=new ArrayList<>();
+        
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
@@ -13,29 +13,28 @@ class Solution {
             int v=edge[1];
             adj.get(u).add(v);
         }
+        
         int[] inc=new int[V];
         for(int i=0;i<V;i++){
             for(int it:adj.get(i)){
                 inc[it]++;
             }
         }
+        Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<V;i++){
-            if(inc[i]==0){
-                q.add(i);
-            }
+            if(inc[i]==0) q.add(i);
         }
         
         while(!q.isEmpty()){
-            int curr=q.poll();
-            ls.add(curr);
-            for(int it:adj.get(curr)){
+            int node=q.poll();
+            ls.add(node);
+            for(int it:adj.get(node)){
                 inc[it]--;
                 if(inc[it]==0){
                     q.add(it);
                 }
             }
         }
-        
         return ls;
     }
 }
