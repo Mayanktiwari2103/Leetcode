3045a9d54369a313348617443b981f0b44b1c85c
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-09-13 19:46:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,51 +1,39 @@
-class Pair{
-    int first;
-    int second;
-    Pair(int first,int second){
-        this.first=first;
-        this.second=second;
-    }
-}
 class Solution {
     public boolean isCycle(int V, int[][] edges) {
+        // Code here
         ArrayList<ArrayList<Integer>> ls=new ArrayList<>();
         for(int i=0;i<V;i++){
             ls.add(new ArrayList<>());
         }
-        for(int[] edge:edges){
+        for(int[] edge: edges){
             int u=edge[0];
             int v=edge[1];
             ls.get(u).add(v);
             ls.get(v).add(u);
         }
-        int[] visited=new int[V];
+        int[] vis=new int[V];
+        
         for(int i=0;i<V;i++){
-            if(visited[i]==0){
-                if(detect(V,ls,i,visited)==true){
+            if(vis[i]==0){
+                if(dfs(i,-1,vis,ls)==true){
                     return true;
                 }
             }
         }
         return false;
-        
     }
     
-    private boolean detect(int V,ArrayList<ArrayList<Integer>> ls,int src,int[] visited){
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(src,-1));
-        visited[src]=1;
-        while(!q.isEmpty()){
-            int node=q.peek().first;
-            int parent=q.peek().second;
-            q.poll();
-            for(int adjnode:ls.get(node)){
-                if(visited[adjnode]==0){
-                    q.add(new Pair(adjnode,node));
-                    visited[adjnode]=1;
-                }
-                else if(adjnode!=parent){
+    private boolean dfs(int start,int parent, int[] vis,ArrayList<ArrayList<Integer>> ls){
+        vis[start]=1;
+        for(int it:ls.get(start)){
+            if(vis[it]==0){
+                vis[it]=1;
+                if(dfs(it,start,vis,ls)==true){
                     return true;
                 }
+            }
+            else if(parent!=it){
+                return true;
             }
         }
         return false;
