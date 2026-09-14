b374129f5e82097d0d75a6cb9514f49d3a8cb0d9
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-09-14 18:13:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
@@ -1,54 +1,55 @@
+
 class Pair{
     int node;
-    int distance;
-    Pair(int node,int distance){
+    int dist;
+    Pair(int node, int dist){
         this.node=node;
-        this.distance=distance;
+        this.dist=dist;
     }
-} 
+}
 class Solution {
     public ArrayList<Integer> dijkstra(int V, int[][] edges, int src) {
         // code here
-        ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
-        for(int i=0;i<V;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int[] edge:edges){
-            int u=edge[0];
-            int v=edge[1];
-            int d=edge[2];
-            adj.get(u).add(new Pair(v,d));
-            adj.get(v).add(new Pair(u,d));
-        }
-        ArrayList<Integer> ls= new ArrayList<>();
-        int[] dist=new int[V];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[src]=0;
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b) -> a.distance-b.distance);
-        pq.add(new Pair(src,0));
-        
-        while(!pq.isEmpty()){
-            Pair curr=pq.poll();
-            int node=curr.node;
-            int distance=curr.distance;
-            
-            if(distance > dist[node]) continue;
-            
-            for(Pair it:adj.get(node)){
-                int u=it.node;
-                int v=it.distance;
-                if(distance+ v < dist[u]){
-                    dist[u]=distance+v;
-                    pq.add(new Pair(u,dist[u]));
-                }
-            }
-            
-        }
-        
-        for(int x:dist){
-            ls.add(x);
-        }
-        return ls;
+       ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
+       ArrayList<Integer> ls=new ArrayList<>();
+       for(int i=0;i<V;i++){
+           adj.add(new ArrayList<>());
+       }
+       for(int[] edge:edges){
+           int u=edge[0];
+           int v=edge[1];
+           int w=edge[2];
+           adj.get(u).add(new Pair(v,w));
+           adj.get(v).add(new Pair(u,w));
+       }
+       int[] distance=new int[V];
+       Arrays.fill(distance,Integer.MAX_VALUE);
+       PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)-> a.dist-b.dist);
+       pq.add(new Pair(src,0));
+       distance[src]=0;
+       
+       while(!pq.isEmpty()){
+           int node=pq.peek().node;
+           int dist=pq.peek().dist;
+           pq.poll();
+           if (dist != distance[node]) {
+              continue;
+           }
+           for(Pair it:adj.get(node)){
+               int v=it.node;
+               int d=it.dist;
+               if(distance[node] + d < distance[v]){
+                   distance[v]=distance[node]+d;
+                   pq.add(new Pair(v,distance[v]));
+               }
+           }
+       }
+       
+       for(int x:distance){
+           ls.add(x);
+       }
+       
+       return ls;
         
     }
 }
