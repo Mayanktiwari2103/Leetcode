a081d9ad091be255507cea7144a138c5ec2ed1e1
Mayanktiwari2103 <167696975+Mayanktiwari2103@users.noreply.github.com>
2026-09-15 18:11:38 +0530

    Time: 31 ms (6.4%), Space: 82.4 MB (83.11%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,34 +1,39 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-        int l = Arrays.stream(bloomDay).min().getAsInt();
-        int r = Arrays.stream(bloomDay).max().getAsInt();
-        if ((long) m * k > n)
-            return -1;
+        int n=bloomDay.length;
+        if((long) m*k > n) return -1;
+        int l=Arrays.stream(bloomDay).min().getAsInt();
+        int r=Arrays.stream(bloomDay).max().getAsInt();
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (possible(bloomDay, m, k, mid) < m) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(cntbouquets(bloomDay,mid,k) < m){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
             }
         }
         return l;
     }
 
-    private int possible(int[] bloomDay, int m, int k, int day) {
-        int n = bloomDay.length;
-        int bouquet = 0;
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if(bloomDay[i] <= day) cnt++;
-            if(cnt==k){
-              bouquet++;
-              cnt=0;
+    private int cntbouquets(int[] bloomDay , int days, int k){
+        int n=bloomDay.length;
+        int bouquet=0;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(cnt<k){
+               if(bloomDay[i] <= days){
+                cnt++;
+               }
+               else{
+                cnt=0;
+               }
             }
-            if(bloomDay[i] > day) cnt=0;
-
+            if(cnt==k){
+               bouquet++;
+               cnt=0;
+            }
         }
         return bouquet;
     }
